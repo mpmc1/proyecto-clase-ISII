c83cb3ebe3fb9a90dd95e6c2629e084c43f76c99
Carlos Puerta <puertam19@gmail.com>
2022-11-07 23:02:04 -0500

    Create budget successfully

--- PersonalBudget_Infraestructure/src/com/mipresupuesto/personalbudget/infraestructure/data/interfaces/BudgetRepository.java
+++ PersonalBudget_Infraestructure/src/com/mipresupuesto/personalbudget/infraestructure/data/interfaces/BudgetRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -15,6 +16,7 @@ public interface BudgetRepository extends JpaRepository<BudgetEntity, UUID>{
 	@Query(value="SELECT * FROM dbo.Budget b WHERE b.idPerson = ?1 AND b.idYear = ?2", nativeQuery = true)
 	Optional<BudgetEntity> findBudgetByPersonAndYear(String personId, String yearId);
 	
+	@Modifying
 	@Query(value="INSERT INTO dbo.Budget(id, idYear, idPerson) VALUES(?1,?2,?3)", nativeQuery = true)
 	void createBudgetByPersonAndYear(String budgetId, String yearId, String personId);
 
